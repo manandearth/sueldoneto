d9eaaa7f61a59cebb2627117aff622617bb29220
manandearth <adamgefen@posteo.net>
2019-04-30 23:01:06 +0200

    Create first few functions

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [orchestra "2018.12.06-2"]
+                 [expound "0.7.2"]]
   :main ^:skip-aot sueldoneto.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/sueldoneto/core.clj
+++ src/sueldoneto/core.clj
@@ -1,7 +1,43 @@
 (ns sueldoneto.core
+  "Calcula el tipo de retención y el salario neto, en función de la situación laboral y familiar y del IRPF vigente."
+  (:require
+   [clojure.spec.alpha :as spec]
+   [orchestra.core :refer [defn-spec]]
+   [orchestra.spec.test :as st]
+   [expound.alpha :as expound])
   (:gen-class))
 
+(def data (atom nil))
+
+(defn get-input
+  "get user input"
+  []
+  (let [input (clojure.string/trim (read-line))]
+    input))
+
+(defn prompt-personal-situation
+  [])
+
+(defn prompt-installments
+  [annual-gross]
+  (println "Número de pagas:")
+  (let [installments (get-input)]
+    (swap! data #(assoc % :installments installments))
+    (println "pagas" (:installments @data) "sueldo anual: " (:annual-gross @data))))
+
+(defn prompt-annual-gross
+  []
+  (println "Sueldo bruto anual:")
+  (let [annual-gross (get-input)]
+    (swap! data #(assoc % :annual-gross annual-gross))
+    (prompt-installments annual-gross)))
+
+(defn start-app []
+  (println "Calculadora de sueldo neto")
+  (println "''''''''''''''''''''''''''")
+  (println "")
+  (prompt-annual-gross))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (start-app))
