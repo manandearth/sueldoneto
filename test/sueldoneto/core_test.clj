52f88c6fe0c8a58f7c4f6923ed5d8086e547ce82
manandearth <adamgefen@posteo.net>
2019-05-01 12:23:44 +0200

    Write `coerce!` test

--- test/sueldoneto/core_test.clj
+++ test/sueldoneto/core_test.clj
@@ -1,7 +1,22 @@
 (ns sueldoneto.core-test
   (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as spec]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.spec.test.alpha :as stest]
             [sueldoneto.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest check-test
+  (testing "check! resilience"
+    (are [expected spec v]
+        (=  (coerce! spec v) expected)
+      12000 :sueldoneto.core/annual-gross          "12000"
+      12    :sueldoneto.core/installments          "12"
+      "A"   :sueldoneto.core/personal-situation    "A"
+      44    :sueldoneto.core/age                   "44"
+      false :sueldoneto.core/contract              "I"
+      "K"   :sueldoneto.core/professional-category "K"
+      )))
+
+(comment
+  (run-tests)
+  )
